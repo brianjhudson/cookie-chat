355c55c8cc9f3f494342a6213625d4ef037a61cf
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-07 14:12:32 -0500

    "Added getMessages function in messageService"

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,14 +1,18 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+
   //In your controller you'll have a getMessages function and a postMessage function, but should be placed on $scope.
 
   //The getMessages function will call the getData method on the messageService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
-
+  var cPromise = messageService.getMessages();
+  cPromise.then(function(results){
+    $scope.messages = results.data;
+    console.log($scope.messages);
+  })
 
 
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will then post it to the backend.
-
 
 
 
--- js/messageService.js
+++ js/messageService.js
@@ -1,10 +1,22 @@
 angular.module('chatroom').service('messageService', function($http){
+  var baseUrl = 'http://practiceapi.devmounta.in/api/chats';
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'http://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
+  this.getMessages = function() {
+    return $http.get(baseUrl).then(function(results){
+      if (results.status === 200)  {
+        console.log(results.data)
+        return results.data
 
+      }
+    }, function() {
+      console.log("error: " + results.error)
+    }
+  )
+  }
 
 
   //On the line below create the postMessage method. This method will add data to the backend server.
